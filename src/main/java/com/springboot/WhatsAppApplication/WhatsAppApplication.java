e0b9ab0d17e20b994a3f5a6ce67982fb657777c7
MOHITE RAJ <mohiteraj647@gmail.com>
2024-09-03 12:39:12 +0530

    modifications of task-B

--- src/main/java/com/springboot/WhatsAppApplication/WhatsAppApplication.java
+++ src/main/java/com/springboot/WhatsAppApplication/WhatsAppApplication.java
@@ -6,10 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WhatsAppApplication {
 
+	public void getData(){
+		int i = 10;
+		// Modification of task-A
+		
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(WhatsAppApplication.class, args);
 	
+		
 	
- 	}
+	}
 
 }
