1b89e34bec9c7578886f7df77f6ef33bc0329b5c
Raj Mohite <59164432+mh-11rajmohite@users.noreply.github.com>
2025-12-09 11:22:10 +0530

    Refactor getHome method to return ResponseEntity

--- src/main/java/com/springboot/WhatsAppApplication/controller/AppController.java
+++ src/main/java/com/springboot/WhatsAppApplication/controller/AppController.java
@@ -1,11 +1,15 @@
 package com.springboot.WhatsAppApplication.controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class AppController {
 
-	public void getHome() {
-		
-		System.out.println("Welcome to home page !!!");
-		
-	}
-	
+	@GetMapping("/")
+public ResponseEntity<String> getHome() {
+    return ResponseEntity.ok("Welcome to home page !!!");
+}
+
 }
